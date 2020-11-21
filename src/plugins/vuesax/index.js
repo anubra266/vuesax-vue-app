b75b5875fd0c5267dee94ec9b713a4ca605454c0
anubra266 <anubra266@gmail.com>
2020-11-21 12:04:09 +0100

    Add Boxicons to vuesax

--- src/plugins/vuesax/index.js
+++ src/plugins/vuesax/index.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import Vuesax from "vuesax";
 import "vuesax/dist/vuesax.css"; //Vuesax styles
-import "boxicons/css/boxicons.min.css"; //Vuesax styles
+import "boxicons/css/boxicons.min.css"; //Box Icons styles
 
 const opts = {
   colors: {
-    dark: "black",
+    // primary: "rgb(255,105,180)",
+    // background:"rgb(0.2, 0.2, 1)"
   },
 };
 
