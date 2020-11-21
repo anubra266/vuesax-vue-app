c7bb31d9e5d97f874c5db1bb16a524008edc3a7e
anubra266 <anubra266@gmail.com>
2020-11-21 12:03:29 +0100

    Create Navba component

--- src/shared/Navbar.vue
+++ src/shared/Navbar.vue
@@ -1,41 +1,51 @@
 <template>
-  <div class="hidden">
-    <vs-navbar shadow square center-collapsed v-model="active">
-      <template #left>
-        <vs-button @click="toggleSidebar" flat icon>
-          <i class="bx bx-menu"></i>
-        </vs-button>
-      </template>
-      <vs-navbar-item :active="active == 'guide'" id="guide">
-        Guide
-      </vs-navbar-item>
-      <vs-navbar-item :active="active == 'docs'" id="docs">
-        Documents
-      </vs-navbar-item>
-      <vs-navbar-item :active="active == 'components'" id="components">
-        Components
-      </vs-navbar-item>
-      <vs-navbar-item :active="active == 'license'" id="license">
-        license
-      </vs-navbar-item>
-      <template #right>
-        <vs-button flat>Login</vs-button>
-        <vs-button>Get Started</vs-button>
-      </template>
-    </vs-navbar>
-  </div>
+  <vs-navbar shadow square center-collapsed v-model="active">
+    <template #left>
+      <vs-button @click="toggleSidebar" class="bar" flat icon>
+        <i class="bx bx-menu"></i>
+      </vs-button>
+    </template>
+    <vs-navbar-item id="home" to="/">
+      Home
+    </vs-navbar-item>
+    <vs-navbar-item id="about" to="/about">
+      About
+    </vs-navbar-item>
+    <!-- <template #right>
+      <vs-button flat>Login</vs-button>
+      <vs-button>Get Started</vs-button>
+    </template> -->
+  </vs-navbar>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapActions } from "vuex";
 
 export default {
   data: () => ({}),
   computed: {
-    ...mapState(["active", "activeSidebar"]),
+    active: {
+      get() {
+        return this.$store.state.active;
+      },
+      set(navbarActive) {
+        return this.$store.dispatch("setActive", navbarActive);
+      },
+    },
   },
   methods: {
     ...mapActions(["toggleSidebar"]),
   },
 };
 </script>
+
+<style scoped>
+.bar {
+  display: none;
+}
+@media (max-width: 767px) {
+  .bar {
+    display: inline-block;
+  }
+}
+</style>
